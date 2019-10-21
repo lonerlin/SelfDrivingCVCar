cccadb9c6d16164276efee0ef9499ddc39f26822
vsnetlin <vsnetlin@163.com>
2019-10-21 21:28:09 +0800

    add carSerial class

---
+++ jetson/carSerial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: LinJungui
+@contact: linjungui@126.com
+@software: garner
+@file: serial.py
+@time: 2019/10/21 0021 20:57
+@desc:CV Car
+'''
+import serial
+class carSerial:
+
+
+
+if __name__=='__main__':
+    pass
