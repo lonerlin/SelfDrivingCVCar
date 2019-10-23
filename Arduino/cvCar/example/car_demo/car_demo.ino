0af088ff6a3b2de045185b3349820bb25a966e7e
lin <linjungui@126.com>
2019-10-23 20:48:41 +0800

    修改了波特率

--- Arduino/cvCar/example/car_demo/car_demo.ino
+++ Arduino/cvCar/example/car_demo/car_demo.ino
@@ -5,7 +5,7 @@
 #define RightDIRPin 7
 #define RightPWMPin 6
 #define Switch 2
-#define BASE_SPEED 75
+#define BASE_SPEED 70
 
 int cur=0;
 
@@ -13,8 +13,8 @@ PID pid(1200,0,0);
 MotorControl mc(LeftDIRPin,LeftPWMPin,RightDIRPin,RightPWMPin);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(13,0);
+  Serial.begin(115200);
+  pinMode(2,INPUT);
 
 }
 
