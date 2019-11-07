17f2e91a52120ef75a2895501fc37ee37c8c2f52
lin <linjungui@126.com>
2019-11-07 11:17:54 +0800

    继续测试巡线，寻找一种稳妥的方式。

--- Arduino/cvCar/example/car_demo/car_demo.ino
+++ Arduino/cvCar/example/car_demo/car_demo.ino
@@ -5,13 +5,13 @@
 #define RightDIRPin 7
 #define RightPWMPin 6
 #define Switch 2
-#define BASE_SPEED 35
-#define FAST_SPEED 60
+#define BASE_SPEED 40
+#define FAST_SPEED 80
 int SPEED=0;
 int cur=0;
 int left,right;
 String inStr="";
-PID pid(1300,0,0);
+PID pid(1500,0,0);
 MotorControl mc(LeftDIRPin,LeftPWMPin,RightDIRPin,RightPWMPin);
 
 void setup() {
@@ -25,7 +25,7 @@ void loop() {
  inStr="";
  while (Serial.available()>0){
     int inChar = Serial.read();
-    Serial.println(inChar);
+    //Serial.println(inChar);
     if(inStr=="" && (char)inChar=='-')inStr="-";
     if (isDigit(inChar)) {
       // convert the incoming byte to a char and add it to the string:
@@ -39,28 +39,29 @@ void loop() {
  }
    
     if(inStr!="")cur=inStr.toInt();
-    Serial.println(cur);
+    //Serial.println(cur);
     if(cur==200)
     {
       mc.Motor(0,0);
     }
     else{
-      if(abs(cur)<15)SPEED=FAST_SPEED;
+      if(abs(cur)<25)SPEED=FAST_SPEED;
+      else if(abs(cur)>60 )SPEED=20;
       else SPEED=BASE_SPEED;
       pid.update(cur);
      
       left=pid.m_command + SPEED;
       right=-pid.m_command + SPEED;
       //left=BASE_SPEED+cur;
-      Serial.print("left:");
-      Serial.println(left);
+      //Serial.print("left:");
+      //Serial.println(left);
       //right=BASE_SPEED-cur;
-      Serial.print("right:");
-      Serial.println(right);
-      //if (left>150)left=180;
-      //if(right>150)right=180;
-      //if(left<-150)left=-180;
-      //if(right<-150)right=-180;
+      //Serial.print("right:");
+      //Serial.println(right);
+      //if (left>150)left=240;
+      //if(right>150)right=240;
+      //if(left<-150)left=-240;
+      //if(right<-150)right=-240;
       
       mc.Motor(left,right);
       delay(5);
