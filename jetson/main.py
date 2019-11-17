703d920b2b0e7d6626d6887fcb10e28a023f5e89
loner lin <linjungui@126.com>
2019-11-17 21:13:48 +0800

    Add main,try to using to processes.

---
+++ jetson/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: LinJungui
+@contact: linjungui@126.com
+@software: garner
+@file: main.py
+@time: 2019/11/17 0017 16:35
+@desc:CV Car
+'''
+from multiprocessing import Process, Manager, Lock
+
+
+def main():
+  pass
+
+
+
+if __name__ == "__main__":
+    main()
