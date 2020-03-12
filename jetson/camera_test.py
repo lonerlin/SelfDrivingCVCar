199413d33e1ac438b930a4b67d907192ae88847d
lin <linjungui@126.com>
2020-03-12 22:32:23 +0800

    test

--- jetson/camera_test.py
+++ jetson/camera_test.py
@@ -6,7 +6,7 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.gstCamera(320, 240, "0")  # using V4L2
 display = jetson.utils.glDisplay()
 
-while display.IsOpen():
+while display.IsOpen( ):
 	img, width, height = camera.CaptureRGBA()
 	detections = net.Detect(img, 320, 240)
 	display.RenderOnce(img, 320, 240)
