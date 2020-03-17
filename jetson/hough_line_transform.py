22382697a20075165d448d1c8f271f626bbadcf2
lin <linjungui@126.com>
2020-03-17 12:10:56 +0800

    添加 直线检查类

---
+++ jetson/hough_line_transform.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+class HoughLines:
+    def __init__(self, rho=1, theta=np.pi/180, threshold=100, min_line_length=50, max_line_gap=1,draw=False):
+        self.rho = rho
+        self.theta = theta
+        self.threshold = threshold
+        self.min_line_length = min_line_length
+        self.max_line_gap = max_line_gap
+        self.draw =draw
+
+    def get_lines(self, frame):
+        lines = cv2.HoughLinesP(frame, self.rho, self.theta, self.threshold, self.min_line_length, self.max_line_gap)
+        if self.draw:
+            tmp_image = frame
+            for x1, y1, x2, y2 in lines[0]:
+                cv2.line(tmp_image, (x1, y1,), (x2, y2), (0, 255, 0), 2)
+            return lines, tmp_image
+        else:
+            return lines, None
+
+
+
