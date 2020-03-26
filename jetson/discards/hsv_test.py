fc5f60d13a3cfb5da60436a3de257355f1e80817
lin <linjungui@126.com>
2020-03-26 23:21:15 +0800

    test hsv

--- jetson/discards/hsv_test.py
+++ jetson/discards/hsv_test.py
@@ -45,58 +45,61 @@ def trackshow(source=1, ksize=5, interv=5 ):
         lower = np.array([hl, ll, sl], dtype=np.uint8)
         upper = np.array([hh, lh, sh], dtype=np.uint8)
         mask = cv2.inRange(frame, lower, upper)
-
+        cv2.imshow("mask", mask)
+        print(np.sum(mask == 255))
         # morphological opening: erode and dilate
-        kernel = np.ones((ksize, ksize), np.uint8)
-        eroded = cv2.erode(mask, kernel)
-        dilated = cv2.dilate(eroded, kernel)
-
-        # morphological closing: dilate and erode
-        dilated = cv2.dilate(dilated, kernel)
-        closed = cv2.erode(dilated, kernel)
-
-        # result: mask & original image:
-        res = cv2.bitwise_and(frame, frame, mask=closed)
-
+        # kernel = np.ones((ksize, ksize), np.uint8)
+        # eroded = cv2.erode(mask, kernel)
+        # dilated = cv2.dilate(eroded, kernel)
+        #
+        # # morphological closing: dilate and erode
+        # dilated = cv2.dilate(dilated, kernel)
+        # closed = cv2.erode(dilated, kernel)
+        #
+        # # result: mask & original image:
+        # res = cv2.bitwise_and(frame, frame, mask=closed)
+        #
+        # x = np.sum(res == 255)
+        # print(x)
         # # get contour
-        _,conts, hier = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # find longest contour
-        maxArea = 0
-        cxs = []
-        cys = []
-        for i in range(len(conts)):
-            if hier[0][i][3] == -1:
-                newArea = cv2.contourArea(conts[i])
-                if newArea > maxArea:
-                    maxcont = conts[i]
-                    maxArea = newArea
-
-        if len(conts) > 0:
-            cv2.drawContours(res, maxcont, -1, (0, 255, 0), 5)
-
-            # centroid position from the moments
-            mom = cv2.moments(maxcont)
-            cx = int(mom['m10'] / mom['m00'])
-            cy = int(mom['m01'] / mom['m00'])
-            # draw centroid
-            cv2.circle(res, (cx, cy), 5, (255, 0, 0), -1)
-
-            # keep track of last 5
-            cxs = [cx] + cxs[1:]
-            cys = [cy] + cys[1:]
-
-        if time.time() - tic > interv:
-            tic = time.time()
-            if len(cxs) > 0:
-                x, y = np.mean(cxs), np.mean(cy)
-                t = time.time() - tic1
-                coords.append([t, x, y])
-                tracker[x, y] += .2
+        # _,conts, hier = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #
+        # # find longest contour
+        # maxArea = 0
+        # cxs = []
+        # cys = []
+        # for i in range(len(conts)):
+        #     if hier[0][i][3] == -1:
+        #         newArea = cv2.contourArea(conts[i])
+        #         if newArea > maxArea:
+        #             maxcont = conts[i]
+        #             maxArea = newArea
+        #
+        # if len(conts) > 0:
+        #     cv2.drawContours(res, maxcont, -1, (0, 255, 0), 5)
+        #
+        #     # centroid position from the moments
+        #     mom = cv2.moments(maxcont)
+        #     cx = int(mom['m10'] / mom['m00'])
+        #     cy = int(mom['m01'] / mom['m00'])
+        #     # draw centroid
+        #     cv2.circle(res, (cx, cy), 5, (255, 0, 0), -1)
+        #
+        #     # keep track of last 5
+        #     cxs = [cx] + cxs[1:]
+        #     cys = [cy] + cys[1:]
+        #
+        # if time.time() - tic > interv:
+        #     tic = time.time()
+        #     if len(cxs) > 0:
+        #         x, y = np.mean(cxs), np.mean(cy)
+        #         t = time.time() - tic1
+        #         coords.append([t, x, y])
+        #         tracker[x, y] += .2
 
         #build a window
         cv2.imshow('original', frame)
-        cv2.imshow('result', res)
+        # cv2.imshow('result', res)
         cv2.imshow('tracker', tracker)
         #
 
