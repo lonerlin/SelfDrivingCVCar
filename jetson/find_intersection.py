0374f14f0d9ef10ed05a5c6e84cf4c32f3f179b9
lin <linjungui@126.com>
2020-03-26 15:47:15 +0800

    find_intersection修改为一个类，有没有必要呢？

--- jetson/find_intersection.py
+++ jetson/find_intersection.py
@@ -3,109 +3,125 @@ import numpy as np
 import math
 from image_init import image_processing
 
-def coordinateFromPoint(origin, angle, radius):
-    """
-        通过圆心位置坐标，角度和半径计算圆上点的坐标
-    :param origin: 圆心位置
-    :param angle: 角度
-    :param radius: 半径
-    :return: 圆周上一个点的坐标
-    """
-    xo = origin[0]
-    yo = origin[1]
+class FindIntersection:
 
-    # Work out the co-ordinate for the pixel on the circumference of the circle
-    x = xo - radius * math.cos(math.radians(angle))
-    y = yo - radius * math.sin(math.radians(angle))
+    def __init__(self,radius, angle=90):
+        self.radius =radius
+        self.angle = angle
 
-    # We only want whole numbers
-    x = int(np.round(x))
-    y = int(np.round(y))
-    return (x, y)
+    def coordinate_from_point(self, origin, angle, radius):
+        """
+            通过圆心位置坐标，角度和半径计算圆上点的坐标
+        :param origin: 圆心位置
+        :param angle: 角度
+        :param radius: 半径
+        :return: 圆周上一个点的坐标
+        """
+        xo = origin[0]
+        yo = origin[1]
 
-def inImageBounds(image, x, y):
-    """
-    判断一个点是否在图片上
-    :param image: 需要检查的图片
-    :param x: 点的X坐标
-    :param y: 点的Y坐标
-    :return: 布尔值
-    """
-    return x >= 0 and y >= 0 and y < len(image) and x < len(image[y])
+        # Work out the co-ordinate for the pixel on the circumference of the circle
+        x = xo - radius * math.cos(math.radians(angle))
+        y = yo - radius * math.sin(math.radians(angle))
 
+        # We only want whole numbers
+        x = int(np.round(x))
+        y = int(np.round(y))
+        return (x, y)
 
-def scan_circle(image,  point, radius, look_angle, display_image=None):
-    x = point[0]
-    y = point[1]
-    scan_start = x - radius
-    scan_end = x + radius
+    def in_image_bounds(self, image, x, y):
+        """
+        判断一个点是否在图片上
+        :param image: 需要检查的图片
+        :param x: 点的X坐标
+        :param y: 点的Y坐标
+        :return: 布尔值
+        """
+        return x >= 0 and y >= 0 and y < len(image) and x < len(image[y])
 
-    endpoint_left = coordinateFromPoint(point, look_angle - 90, radius)
-    endpoint_right = coordinateFromPoint(point, look_angle + 90, radius)
+    def scan_circle(self, image,  point, radius, look_angle, display_image=None):
+        """
+            以指定的圆心，半径，方向，在图上画出半圆，并返回半圆数据集
+        :param image: 输入的图像
+        :param point: 圆心坐标
+        :param radius: 半径
+        :param look_angle: 方向（查找的范围是该方向的正负90度）
+        :param display_image: 需要渲染的图像
+        :return: 返回数据集[角度，圆圈上点的值，坐标X轴，坐标Y轴]
+        """
+        x = point[0]
+        y = point[1]
+        scan_start = x - radius
+        scan_end = x + radius
 
-    # print("scanline left:{} right:{} angle:{}".format(endpoint_left, endpoint_right, look_angle))
-    if not (display_image is None):
-        # Draw a circle to indicate where we start and end scanning.
-        cv2.circle(display_image, (endpoint_left[0], endpoint_left[1]), 5, (255, 100, 100), -1, 8, 0)
-        cv2.circle(display_image, (endpoint_right[0], endpoint_right[1]), 5, (100, 255, 100), -1, 8, 0)
-        cv2.line(display_image, (endpoint_left[0], endpoint_left[1]), (endpoint_right[0], endpoint_right[1]),
-                 (255, 0, 0), 1)
-        cv2.circle(display_image, (x, y), radius, (100, 100, 100), 1, 8, 0)
+        endpoint_left = self.coordinate_from_point(point, look_angle - 90, radius)
+        endpoint_right = self.coordinate_from_point(point, look_angle + 90, radius)
 
-        # We are only going to scan half the circumference
-    data = np.zeros(shape=(180, 4))
+        # print("scanline left:{} right:{} angle:{}".format(endpoint_left, endpoint_right, look_angle))
+        if not (display_image is None):
+            # Draw a circle to indicate where we start and end scanning.
+            cv2.circle(display_image, (endpoint_left[0], endpoint_left[1]), 5, (255, 100, 100), -1, 8, 0)
+            cv2.circle(display_image, (endpoint_right[0], endpoint_right[1]), 5, (100, 255, 100), -1, 8, 0)
+            cv2.line(display_image, (endpoint_left[0], endpoint_left[1]), (endpoint_right[0], endpoint_right[1]),
+                     (255, 0, 0), 1)
+            cv2.circle(display_image, (x, y), radius, (100, 100, 100), 1, 8, 0)
 
-    # Getting the co-ordinates and value for every degree in the semi circle
-    startAngle = look_angle - 90
+            # We are only going to scan half the circumference
+        data = np.zeros(shape=(180, 4))
 
-    returnVal = True
-    for i in range(0, 180, 1):
-        current_angle = startAngle + i
-        scan_point = coordinateFromPoint(point, current_angle, radius)
+        # Getting the co-ordinates and value for every degree in the semi circle
+        startAngle = look_angle - 90
 
-        if inImageBounds(image, scan_point[0], scan_point[1]):
-            imageValue = image[scan_point[1]][scan_point[0]]
-            data[i] = [i, imageValue, scan_point[0], scan_point[1]]
-        else:
-            returnVal = False
-            break
-    return returnVal, data
+        returnVal = True
+        for i in range(0, 180, 1):
+            current_angle = startAngle + i
+            scan_point = self.coordinate_from_point(point, current_angle, radius)
 
+            if self.in_image_bounds(image, scan_point[0], scan_point[1]):
+                imageValue = image[scan_point[1]][scan_point[0]]
+                data[i] = [i, imageValue, scan_point[0], scan_point[1]]
+            else:
+                returnVal = False
+                break
+        return returnVal, data
 
-def findInCircle(scan_data):
+    def find_in_circle(self, scan_data):
+        """
+        输入的圆的数据集进行分析，并找出路口的特征点，此处输入的点集必须为二值形式
+        :param scan_data:  scan_circle返回的数据集
+        :return: 返回路口的特征点集[角度，坐标X，坐标Y]
+        """
+        tmp_count = 0
+        angle_list = []
+        for i in range(len(scan_data)):
+            if scan_data[i][1] == 0:
+                if tmp_count > 0:
+                        angle_list.append(scan_data[i-int(tmp_count/2)])
+                        tmp_count = 0
+            else:
+                tmp_count += 1
 
-    tmp_count = 0
-    angle_list = []
-    for i in range(len(scan_data)):
-        if scan_data[i][1] == 0:
-            if tmp_count > 0:
-                    angle_list.append(scan_data[i-int(tmp_count/2)])
-                    tmp_count = 0
-        else:
-            tmp_count += 1
+        return angle_list
 
-    return angle_list
-
-
-def find(image, point, rander_image=None):
-    _,scan_data = scan_circle(image, point, radius=150, look_angle=90, display_image=rander_image)
-    road = findInCircle(scan_data)
-    return_value = []
-    for data in road:
-        return_value.append([int(data[0]), int(data[2]), int(data[3])])
-    if len(return_value) > 0 and not (rander_image is None):
-        for end_point in return_value:
-            cv2.arrowedLine(rander_image,point, (end_point[1], end_point[2]), color=(255, 0, 0), thickness=3)
-    return  return_value
+    def find(self,image, point, rander_image=None):
+        _,scan_data = self.scan_circle(image, point, self.radius, look_angle=self.angle, display_image=rander_image)
+        road = self.find_in_circle(scan_data)
+        return_value = []
+        for data in road:
+            return_value.append([int(data[0]), int(data[2]), int(data[3])])
+        if len(return_value) > 0 and not (rander_image is None):
+            for end_point in return_value:
+                cv2.arrowedLine(rander_image, point, (end_point[1], end_point[2]), color=(0, 255, 0), thickness=1)
+        return  return_value
 
 
 if __name__ == '__main__':
-    image = cv2.imread('h:/3.jpg')
+    image = cv2.imread('h:/2.jpg')
     image2 = image_processing(image, width=320, height=240, convert_type="BINARY", bitwise_not=True)
     cv2.imshow("test_one", image2)
-
+    fi= FindIntersection(150)
     #data = find(image2, (160, 230), image)
-    data2 = find(image2, (160, 200), image)
+    data2 = fi.find(image2, (160, 200), image)
 
     #print(data)
     print(data2)
