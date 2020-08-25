81158837fa3c41f50e44107237123572179afb19
lin <linjungui@126.com>
2020-08-25 18:23:58 +0800

    rename

--- jetson/audio/audio.py
+++ jetson/audio/audio.py
@@ -1,6 +1,7 @@
 import pyaudio
 import wave
 from tqdm import tqdm
+import time
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
@@ -39,3 +40,31 @@ def record(filename, record_seconds):
     wf.setframerate(RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
+
+
+def play(filename):
+
+    wf = wave.open(filename, 'rb')
+    # instantiate PyAudio (1)
+    p = pyaudio.PyAudio()
+
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
+        return data, pyaudio.paContinue
+
+    # open stream (2)
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True,
+                    stream_callback=callback)
+    # read data
+    stream.start_stream()
+    while stream.is_active():
+        time.sleep(0.1)
+    # stop stream (4)
+    stream.stop_stream()
+    stream.close()
+    # close PyAudio (5)
+    p.terminate()
+
