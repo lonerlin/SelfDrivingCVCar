40a495afba3daca459b409183ba7d03c911285d8
lin <linjungui@126.com>
2020-08-29 23:28:39 +0800

    添加人脸识别类。

---
+++ jetson/od/face_recognition.py
@@ -0,0 +1,14 @@
+# _*_coding:utf-8 _*_
+# @Time　　:2020/8/29 0029   下午 11:25
+# @Author　 : Loner Lin
+# @File　　  :face_recognition.py
+# @Software  :PyCharm
+
+import face_recognition
+import cv2
+import numpy as np
+
+
+class FaceRecognition:
+    def __init__(self):
+        pass
