4ca000098588c5a20dd2bb984afd2475ccb3d7d9
lin <linjungui@126.com>
2020-09-12 11:35:56 +0800

    fix bugs

--- microbit/bridge.py
+++ microbit/bridge.py
@@ -5,12 +5,11 @@ import DFMotor
 import DFServo
 import math
 
-motor =DFMotor()
-
+# motor =DFMotor()
 
 
 receive_string = ""
-is_send = True
+# is_send = True
 stop = False
 uart.init(baudrate=115200)
 button_a_state = False
@@ -18,73 +17,74 @@ button_b_state = False
 
 
 def msg_decode(message):
-    temp = message.strip().replace("\n", "").replace("\r", "")
-    if len(temp) == 8:
-        if temp[0] == "0" or temp[0] == "1":
-            set_motor(temp)
-        elif temp[0] == "2":
-            servo(temp)
-        elif temp[0] == "3":
-            digital_read_write(temp)
-        elif temp[0] == "4":
-            analog_read_write(temp)
-        else:
-            other_operate(temp)
+    # temp = message.strip().replace("\n", "").replace("\r", "")
+    _send_msg("3" + message[1:8])
+    # if len(temp) == 8:
+    #     if temp[0] == "0" or temp[0] == "1":
+    #         set_motor(temp)
+    #     elif temp[0] == "2":
+    #         servo(temp)
+    #     elif temp[0] == "3":
+    #         digital_read_write(temp)
+    #     elif temp[0] == "4":
+    #         analog_read_write(temp)
+    #     else:
+    #         other_operate(temp)
 
 
-def set_motor(speed_message):
-    one_motor(1, speed_message[:4])
-    one_motor(3, speed_message[4:])
+# def set_motor(speed_message):
+#     one_motor(1, speed_message[:4])
+#     one_motor(3, speed_message[4:])
 
 
-def one_motor(motor_id, motor_message):
-    direction = None
-    if motor_message[0] == "0":
-        direction = motor.cw
-    else:
-        direction = motor.ccw
-    motor.speed(int(motor_message[1:]))
-    motor.run(motor_id, direction)
+# def one_motor(motor_id, motor_message):
+#     direction = None
+#     if motor_message[0] == "0":
+#         direction = motor.cw
+#     else:
+#         direction = motor.ccw
+#     motor.speed(int(motor_message[1:]))
+#     motor.run(motor_id, direction)
 
 
-def servo(servo_message):
-    servo_id = int(servo_message[4:])
-    angle = int(servo_message[1:5])
-    s = DFServo(servo_id)
-    s.angle(angle)
+# def servo(servo_message):
+#     servo_id = int(servo_message[4:])
+#     angle = int(servo_message[1:5])
+#     s = DFServo(servo_id)
+#     s.angle(angle)
 
 
-def digital_read_write(digital_message):
-    if digital_message[3:5] != "99":
-        pin = _get_pin(digital_message[3:5])
-        _send_msg(str(pin.read_digital()) + "\n")
+# def digital_read_write(digital_message):
+#     if digital_message[3:5] != "99":
+#         pin = _get_pin(digital_message[3:5])
+#         _send_msg(str(pin.read_digital()) + "\n")
 
-    if digital_message[5:7] != "99":
-        pin = _get_pin(digital_message[5:7])
-        pin.write_digital(int(digital_message[7]))
-
-
-def _get_pin(pin_index_string):
-    pin_number = int(pin_index_string)
-    pin = eval("pin{}".format(pin_number))
-    return pin
+#     if digital_message[5:7] != "99":
+#         pin = _get_pin(digital_message[5:7])
+#         pin.write_digital(int(digital_message[7]))
 
 
-def analog_read_write(analog_message):
-    if analog_message[1:3] != "99":
-        pin = _get_pin(analog_message[1:3])
-        _send_msg(str(_map(pin.read_analog(), 0, 1024, 0, 255)) + "\n")
-    if analog_message[3:5] != "99":
-        pin = _get_pin(analog_message[3:5])
-        pin.write_analog(_map(int(analog_message[5:], 0, 255, 0, 1024)))
+# def _get_pin(pin_index_string):
+#     pin_number = int(pin_index_string)
+#     pin = eval("pin{}".format(pin_number))
+#     return pin
 
 
-def other_operate(message):
-    pass
+# def analog_read_write(analog_message):
+#     if analog_message[1:3] != "99":
+#         pin = _get_pin(analog_message[1:3])
+#         _send_msg(str(_map(pin.read_analog(), 0, 1024, 0, 255)) + "\n")
+#     if analog_message[3:5] != "99":
+#         pin = _get_pin(analog_message[3:5])
+#         pin.write_analog(_map(int(analog_message[5:], 0, 255, 0, 1024)))
 
 
-def _map(x, in_min, in_max, out_min, out_max):
-    return (x-in_min)*(out_max-out_min)/(in_max-in_min)+out_min
+# def other_operate(message):
+#     pass
+
+
+# def _map(x, in_min, in_max, out_min, out_max):
+#     return (x-in_min)*(out_max-out_min)/(in_max-in_min)+out_min
 
 
 def _send_msg(message):
@@ -105,17 +105,16 @@ def check_button():
         _send_msg("30000111")
         button_b_state = True
     else:
-        if button_a_state:
+        if button_b_state:
             _send_msg("30000110")
             button_b_state = False
 
 
 while not stop:
     if uart.any():
-        receive_string = uart.readline()
+        receive_string = str(uart.readline())
+
     if len(receive_string) > 0:
         msg_decode(receive_string)
         receive_string = ""
-        check_button()
-
-
+    check_button()
