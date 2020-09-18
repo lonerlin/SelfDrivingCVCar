a430dc787484ef3c589073fa717c42ea7310d225
lin <linjungui@126.com>
2020-09-18 19:59:32 +0800

    完善了风扇程序。

--- microbit/mini_bridge.py
+++ microbit/mini_bridge.py
@@ -11,14 +11,18 @@ motor = DFMotor()
 DFServoInit = 0
 S1 = DFServo(1)
 S1.angle(90)
-while not stop:
+while True:
     if uart.any():
         rs = str(uart.readline())
         t = rs.strip().replace("\n", "")
-    if len(t) == 8:
-        if t[0] == 0:
-            motor.speed(int(t[1:4]))
-            motor.run(1, motor.CW)
-        if t[0] == 2:
-            S1.angle(int(t[1:4]))
-
+    if not stop:
+        if len(t) == 8:
+            if t[0] == 0:
+                motor.speed(int(t[1:4]))
+                motor.run(1, motor.CW)
+            if t[0] == 2:
+                S1.angle(int(t[1:4]))
+    if button_a.is_pressed():
+        stop = False
+    if button_b.is_pressed():
+        stop = True
